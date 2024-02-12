df1c2d5c805d8549a5c87c384e419d20df414c24
Hecan Zhou <hecan12345@gmail.com>
2024-02-12 14:01:17 -0500

    Add basic template for our project propoasal, batch file works for foxit reader

--- latexGen.bat
+++ latexGen.bat
@@ -2,10 +2,14 @@ rm -f APS360_Project.pdf
 
 cd latexForSubmission
 
+taskkill /im "FoxitPDFReader.exe" /f
+
 pdflatex APS360_Project.tex
 
 copy APS360_Project.pdf ..
 
 cd ..
+
+start "" "C:\Program Files (x86)\Foxit Software\Foxit PDF Reader\FoxitPDFReader.exe" "APS360_Project.pdf"
 
 echo done and copied
